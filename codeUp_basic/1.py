b937caad48e7637e32efd294366cd193dfd1c855
Gahee Yang <gahee9006@gmail.com>
2024-05-27 10:02:29 +0900

    acronym 머리글자어 만들기

--- codeUp_basic/1.py
+++ codeUp_basic/1.py
@@ -1,1 +1,9 @@
-print("I like green and yellow")
+#머리 글자어 만들기
+phrase = input()
+
+acronym = " "
+
+for word in phrase.upper().split(): #다 대문자로 만들어서 공백 기준으로 자른다
+    acronym += word[0]
+
+print(acronym)
